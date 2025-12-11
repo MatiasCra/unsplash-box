2f8e3701343c34b9a2610ce69f9404afbf36b266
MatiasCra <cravchikm@gmail.com>
2025-12-11 11:59:06 -0300

    force dynamic collections

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   title: "Collections",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const collections = await fetchCollections();
 
